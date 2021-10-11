4414966b67eb5aca59e238c5a3e8f6d6a593c873
svetlana-rezvaya <svetlana-2305@mail.ru>
2021-10-11 21:55:09 +0300

    Add the Dish structure

--- food.go
+++ food.go
@@ -1,5 +1,11 @@
 package food
 
+// Dish ...
+type Dish struct {
+	Weight      int
+	EnergyValue int
+}
+
 // CountCalories ...
 func CountCalories(weight int, energyValue int) int {
 	return weight * energyValue / 100
