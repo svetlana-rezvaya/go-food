04ec4211200d585c2c37d55f12b34a3056e6a7a0
svetlana-rezvaya <svetlana-2305@mail.ru>
2021-10-11 22:25:18 +0300

    Add the CountTotalCalories function

--- food.go
+++ food.go
@@ -10,3 +10,14 @@ type Dish struct {
 func CountCalories(weight int, energyValue int) int {
 	return weight * energyValue / 100
 }
+
+// CountTotalCalories ...
+func CountTotalCalories(dishes []Dish) int {
+	totalCalories := 0
+	for _, dish := range dishes {
+		calories := CountCalories(dish.Weight, dish.EnergyValue)
+		totalCalories = totalCalories + calories
+	}
+
+	return totalCalories
+}
