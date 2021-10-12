712d9a2f8418830ef57e167cdaa8b5e8f969a426
svetlana-rezvaya <svetlana-2305@mail.ru>
2021-10-12 21:30:13 +0300

    Add the Dish.CountMacros method

--- food.go
+++ food.go
@@ -4,11 +4,22 @@ package food
 type Dish struct {
 	Weight      int
 	EnergyValue int
+	Protein     int
+	Fat         int
+	Carbs       int
 }
 
 // CountCalories ...
 func (dish Dish) CountCalories() int {
 	return dish.Weight * dish.EnergyValue / 100
+}
+
+// CountMacros ...
+func (dish Dish) CountMacros() (int, int, int) {
+	protein := dish.Weight * dish.Protein / 100
+	fat := dish.Weight * dish.Fat / 100
+	carbs := dish.Weight * dish.Carbs / 100
+	return protein, fat, carbs
 }
 
 // CountTotalCalories ...
