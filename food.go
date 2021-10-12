857e919aa5fd26107d409f308b16bf5c2a750c7c
svetlana-rezvaya <svetlana-2305@mail.ru>
2021-10-12 21:56:05 +0300

    Add the Macros structure

--- food.go
+++ food.go
@@ -9,17 +9,24 @@ type Dish struct {
 	Carbs       int
 }
 
+// Macros ...
+type Macros struct {
+	Protein int
+	Fat     int
+	Carbs   int
+}
+
 // CountCalories ...
 func (dish Dish) CountCalories() int {
 	return dish.Weight * dish.EnergyValue / 100
 }
 
 // CountMacros ...
-func (dish Dish) CountMacros() (int, int, int) {
+func (dish Dish) CountMacros() Macros {
 	protein := dish.Weight * dish.Protein / 100
 	fat := dish.Weight * dish.Fat / 100
 	carbs := dish.Weight * dish.Carbs / 100
-	return protein, fat, carbs
+	return Macros{Protein: protein, Fat: fat, Carbs: carbs}
 }
 
 // CountTotalCalories ...
@@ -34,16 +41,16 @@ func CountTotalCalories(dishes []Dish) int {
 }
 
 // CountTotalMacros ...
-func CountTotalMacros(dishes []Dish) (int, int, int) {
+func CountTotalMacros(dishes []Dish) Macros {
 	totalProtein := 0
 	totalFat := 0
 	totalCarbs := 0
 	for _, dish := range dishes {
-		protein, fat, carbs := dish.CountMacros()
-		totalProtein = totalProtein + protein
-		totalFat = totalFat + fat
-		totalCarbs = totalCarbs + carbs
+		macros := dish.CountMacros()
+		totalProtein = totalProtein + macros.Protein
+		totalFat = totalFat + macros.Fat
+		totalCarbs = totalCarbs + macros.Carbs
 	}
 
-	return totalProtein, totalFat, totalCarbs
+	return Macros{Protein: totalProtein, Fat: totalFat, Carbs: totalCarbs}
 }
