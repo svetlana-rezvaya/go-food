5b3454ba33b5eaa90d653ed2d2899fe5568bb797
svetlana-rezvaya <svetlana-2305@mail.ru>
2021-10-19 21:51:56 +0300

    Move the Macros structure to an independent file

--- food.go
+++ food.go
@@ -1,20 +1,5 @@
 package food
 
-// Macros ...
-type Macros struct {
-	Protein float64
-	Fat     float64
-	Carbs   float64
-}
-
-// Add ...
-func (macros Macros) Add(anotherMacros Macros) Macros {
-	protein := macros.Protein + anotherMacros.Protein
-	fat := macros.Fat + anotherMacros.Fat
-	carbs := macros.Carbs + anotherMacros.Carbs
-	return Macros{Protein: protein, Fat: fat, Carbs: carbs}
-}
-
 // Dish ...
 type Dish struct {
 	Weight      float64
