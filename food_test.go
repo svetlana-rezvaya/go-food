004c6c43a50729771acfaca33699414a06120110
svetlana-rezvaya <svetlana-2305@mail.ru>
2021-11-04 21:12:53 +0300

    Add the tests for the CountTotalCalories function

--- food_test.go
+++ food_test.go
@@ -30,3 +30,32 @@ func TestCountTotalWeight_nonEmpty(test *testing.T) {
 		test.Fail()
 	}
 }
+
+func TestCountTotalCalories_empty(test *testing.T) {
+	receivedTotalCalories := CountTotalCalories([]Dish{})
+
+	wantedTotalCalories := 0.0
+	if receivedTotalCalories != wantedTotalCalories {
+		test.Logf(
+			"failed:\n  expected: %+v\n  actual: %+v",
+			wantedTotalCalories,
+			receivedTotalCalories,
+		)
+		test.Fail()
+	}
+}
+
+func TestCountTotalCalories_nonEmpty(test *testing.T) {
+	dishes := []Dish{mockDish{calories: 2.3}, mockDish{calories: 4.2}}
+	receivedTotalCalories := CountTotalCalories(dishes)
+
+	wantedTotalCalories := 6.5
+	if receivedTotalCalories != wantedTotalCalories {
+		test.Logf(
+			"failed:\n  expected: %+v\n  actual: %+v",
+			wantedTotalCalories,
+			receivedTotalCalories,
+		)
+		test.Fail()
+	}
+}
