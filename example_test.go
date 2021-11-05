fa37416a167fbd2e75afb87cc61c1f47d87245fd
svetlana-rezvaya <svetlana-2305@mail.ru>
2021-11-05 21:51:43 +0300

    Add the example

--- example_test.go
+++ example_test.go
@@ -1,27 +1,4 @@
-# go-food
-
-[![GoDoc](https://godoc.org/github.com/svetlana-rezvaya/go-food?status.svg)](https://godoc.org/github.com/svetlana-rezvaya/go-food)
-[![Go Report Card](https://goreportcard.com/badge/github.com/svetlana-rezvaya/go-food)](https://goreportcard.com/report/github.com/svetlana-rezvaya/go-food)
-[![Build Status](https://app.travis-ci.com/svetlana-rezvaya/go-food.svg?branch=master)](https://app.travis-ci.com/svetlana-rezvaya/go-food)
-[![codecov](https://codecov.io/gh/svetlana-rezvaya/go-food/branch/master/graph/badge.svg)](https://codecov.io/gh/svetlana-rezvaya/go-food)
-
-The library for calculating calories and macronutrients (protein, carbohydrates, and fat) of specified dishes.
-
-## Problems
-
-- [Problem 1](docs/problem_1.md) (RU)
-- [Problem 2](docs/problem_2.md) (RU)
-
-## Installation
-
-```
-$ go get github.com/svetlana-rezvaya/go-food
-```
-
-## Usage Example
-
-```go
-package main
+package food_test
 
 import (
 	"fmt"
@@ -35,7 +12,7 @@ func roundToPrecision(value float64, precision int) float64 {
 	return math.Round(value*factor) / factor
 }
 
-func main() {
+func Example() {
 	// the calories and macronutrients are taken from https://fdc.nal.usda.gov/
 	dishes := []food.Dish{
 		// buckwheat grain
@@ -114,10 +91,3 @@ func main() {
 	// calories: 684.24
 	// macros: {Protein:55.57 Fat:27.43 Carbs:56.9}
 }
-```
-
-## License
-
-The MIT License (MIT)
-
-Copyright &copy; 2021 svetlana-rezvaya
